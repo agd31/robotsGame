c985cdadbb865000dcd74beb2db332139a2bfaac
Alvaro Garcia <agd3101@gmail.com>
2019-03-04 20:35:14 +0100

    Tercera subida, se mueve

--- starter-code/scripts/player.js
+++ starter-code/scripts/player.js
@@ -41,16 +41,16 @@ Player.prototype.movePlayer = function() {
 
   /*Derecha, la primera lo mueve la segunda lo para*/
   if(this.isMovingRight === true && this.x+1 + this.width <= canvas.width){this.x += 1};
-  if(this.isMovingRight === true && this.x==canvas.width-1){this.isMovingRight === false};
+  if(this.isMovingRight === true && this.x+this.width==canvas.width-1){this.isMovingRight = false};
   /*Izquierda*/
   if(this.isMovingLeft === true && this.x-1 > 0){this.x -= 1};
-  if(this.isMovingLeft === true && this.x==1){this.isMovingLeft === false};
+  if(this.isMovingLeft === true && this.x==1){this.isMovingLeft = false};
   /*Arriba*/
   if(this.isMovingUp === true && this.y-1 >= 0){this.y -= 1};
-  if(this.isMovingUp === true && this.y==1){this.isMovingUp === false};
+  if(this.isMovingUp === true && this.y==1){this.isMovingUp = false};
   /*Abajo*/
   if(this.isMovingDown === true && this.y+1 + this.height <= canvas.height){this.y += 1};
-  if(this.isMovingDown === true && this.y==canvas.height-1){this.isMovingDown ===false}
+  if(this.isMovingDown === true && this.y+canvas.height==canvas.height-1){this.isMovingDown =false}
   /*if(this.x=canvas.width){this.isMovingRight===false};
   if(this.x=1){this.isMovingLeft===false};*/
   
